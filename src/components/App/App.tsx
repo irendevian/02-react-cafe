87f9862e03b43181bc7e28131c692965a356332b
Iryna Devianina <devianinairen@gmail.com>
2025-09-01 23:41:54 +0300

    start add components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import css from './App.module.css'
+import CafeInfo from '../CafeInfo/CafeInfo'
+
+function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
+}
+
+export default App;
